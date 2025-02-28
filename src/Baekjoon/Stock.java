f585e012bcd1d357acf2a3c822e9b16f8f06178b
nam-kiyun <fordnam1@gmail.com>
2025-02-28 17:39:13 +0900

    #27 주식 풀이

--- src/Baekjoon/Stock.java
+++ src/Baekjoon/Stock.java
@@ -12,30 +12,18 @@ public class Stock {
         for(int k=0;k<T;k++){
             int max = 0;
             long result = 0;
-            int count = 0;
-            int m =0;
             int N = Integer.parseInt(br.readLine());
             int[] arr=new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int i=0;i<N;i++){
                 arr[i]=Integer.parseInt(st.nextToken());
             }
-            while (count != N) {
-                for(int i=count;i<N;i++) {
-                    if(max<arr[i]) {
-                        max=arr[i];
-                    }
-                }
-                for(int i=count;i<N;i++) {
-                    if (arr[i] == max) {
-                        result += (i - count) * max - m;
-                        count = i + 1;
-                        m = 0;
-                        max = 0;
-                        break;
-                    } else {
-                        m += arr[i];
-                    }
+
+            for(int j=(arr.length-1);j>=0;j--) {
+                if(max<arr[j]) {
+                    max=arr[j];
+                } else {
+                    result += max - arr[j];
                 }
             }
             sb.append(result).append("\n");
