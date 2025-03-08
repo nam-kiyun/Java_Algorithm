c6b38a760218e458c4fc2a8b09b4e20c90c83866
nam-kiyun <fordnam1@gmail.com>
2025-03-08 23:07:06 +0900

    #35 균형잡힌 세상 풀이

---
+++ src/Baekjoon/BalancedWorld.java
@@ -0,0 +1,45 @@
+package Baekjoon;
+import java.io.*;
+import java.util.*;
+
+public class BalancedWorld {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String str = br.readLine();
+            if (str.equals(".")) break;
+
+            Stack<String> stack = new Stack<>();
+
+            String[] sentence = str.split("");
+            for(String word : sentence){
+                switch (word) {
+                    case ("("):
+                    case ("["):
+                        stack.push(word);
+                        break;
+                    case (")"):
+                        if(!stack.isEmpty() && stack.peek().equals("("))
+                            stack.pop();
+                        else stack.push(")");
+                        break;
+                    case ("]"):
+                        if(!stack.isEmpty() && stack.peek().equals("["))
+                            stack.pop();
+                        else stack.push("]");
+                        break;
+                }
+            }
+
+            if (stack.empty())
+                sb.append("yes\n");
+            else
+                sb.append("no\n");
+
+        }
+
+        System.out.println(sb);
+    }
+}
